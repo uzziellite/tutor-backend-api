53daa14cdd636c42e3642f6b0abbc8ad6fc7d4eb
Uzziel <uzzielkk@gmail.com>
2022-09-11 22:07:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Connector
+
+There is nothing much to say about this project except that it is meant to
+connect a frontend interface with a directus backend
+
+There is nothing cool about it. Sorry for dissapointing you.
---
+++ api/index.js
@@ -0,0 +1,63 @@
+const app = require('express')()
+const Directus = require('@directus/sdk')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const dotenv = require('dotenv')
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 60 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Set path to .env file
+dotenv.config({ path: './.env' })
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+
+//Allow requests from any origin
+app.use(cors())
+
+//Protect against common vulnerabilities
+app.use(helmet())
+
+// custom 404
+app.use((req, res, next) => {
+  res.status(404).send('Ooopsie dooopsie, there is nothing here')
+})
+
+const directus = new Directus(process.env.BACKEND_URL,{
+  auth:{
+    staticToken: process.env.API_KEY
+  }
+})
+
+//Send an email invitation to the user to join the project
+app.post('/api/tutor-invite', async(req, res) => {
+  const email = req.body.email
+  await directus.users.invites.send(email, process.env.TUTOR_ROLE).then(() => {
+    
+    const response = {
+      type: "success",
+      message: "Please check your email address to finish setting up your account"
+    }
+
+    res.json(response)
+
+  }).catch((err) => {
+    
+    const response = {
+      type: "error",
+      message: "Unable to register your tutor account. Please try again"
+    }
+    
+    res.json(response)
+
+  })
+
+})
+
+module.exports = app
